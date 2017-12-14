98d9f58701e13c218236395c164a98e2111124c2
Yong Chee <yong.chee@sap.com>
2017-12-14 16:45:27 -0500

    Component Architecture - Feature module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    PassengerDashboardModule
   ],
   bootstrap: [
     AppComponent
