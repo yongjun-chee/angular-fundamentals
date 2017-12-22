0bf3f9b231115658808c3fa880e78c667aa9d8f3
Yong Chee <yong.chee@sap.com>
2017-12-22 11:53:59 -0500

    Template-driven Forms, Inputs and Validation - Binding to radio buttons

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -22,10 +22,47 @@ import { Passenger } from '../../models/passenger.interface';
           name="id"
           [ngModel]="detail?.id">
       </div>
+
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="checkedIn"
+            [value]="true"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+          Yes
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="checkedIn"
+            [value]="false"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+          No
+        </label>
+      </div>
+
+      <div *ngIf="form.value.checkedIn">
+        Check-In Date: 
+        <input 
+          type="number"
+          name="checkInDate"
+          [ngModel]="detail?.checkInDate">
+      </div>
+
       {{ form.value | json }}
     </form>
     `
 })
 export class PassengerFormComponent {
   @Input() detail: Passenger;
+
+  toggleCheckIn(checkedIn: boolean) {
+    if (checkedIn) {
+      this.detail.checkedIn = true;
+      this.detail.checkInDate = Date.now();
+    } 
+  }
 }
