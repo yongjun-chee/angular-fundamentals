3fc465784d63b62187c9b12077fefb42000a9faf
Yong Chee <yong.chee@sap.com>
2017-12-22 15:45:54 -0500

    Template-driven Forms, Inputs and Validation - <select> option rendering, and ngValue

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
 
 @Component({
   selector: "passenger-form",
@@ -41,6 +42,20 @@ import { Passenger } from '../../models/passenger.interface';
           [ngModel]="detail?.checkInDate">
       </div>
 
+      <div>
+        Luggage:
+        <select
+          name="baggage"
+          [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage"
+            [value]="item.key"
+            [selected]="item.key === detail?.baggage">
+            {{item.value}}
+          </option>
+        </select>
+      </div>
+
       {{ form.value | json }}
     </form>
     `
@@ -48,10 +63,29 @@ import { Passenger } from '../../models/passenger.interface';
 export class PassengerFormComponent {
   @Input() detail: Passenger;
 
+  baggage: Baggage[] = [
+    {
+      key: "none",
+      value: "No baggage"
+    },
+    {
+      key: "hand-only",
+      value: "Hand baggage"
+    },
+    {
+      key: "hold-only",
+      value: "Hold baggage"
+    },
+    {
+      key: "hand-hold",
+      value: "Hand and hold baggage"
+    }
+  ];
+
   toggleCheckIn(checkedIn: boolean) {
     if (checkedIn) {
       this.detail.checkedIn = true;
       this.detail.checkInDate = Date.now();
-    } 
+    }
   }
 }
