9b7c7ae24fe81a724d6e18f02fdb7b41caae0134
Yong Chee <yong.chee@sap.com>
2017-12-22 00:56:55 -0500

    Template-driven Forms, Inputs and Validation - ngForm and ngModel

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -3,20 +3,29 @@ import { Component, Input } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
-  selector: 'passenger-form',
-  styleUrls: ['passenger-form.component.scss'],
+  selector: "passenger-form",
+  styleUrls: ["passenger-form.component.scss"],
   template: `
-    <form>
-      Form!
+    <form #form="ngForm" novalidate>
+      {{ detail | json }}
       <div>
-        {{ detail | json }}
+        Passenger name:
+        <input
+          type="text"
+          name="fullname"
+          [ngModel]="detail?.fullname">
       </div>
+      <div>
+        Passenger ID:
+        <input
+          type="number"
+          name="id"
+          [ngModel]="detail?.id">
+      </div>
+      {{ form.value | json }}
     </form>
     `
 })
-
 export class PassengerFormComponent {
-  @Input()
-  detail: Passenger;
-
+  @Input() detail: Passenger;
 }
