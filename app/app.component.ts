ca7061c0d0900cca649a8a070cdbe3a2d0b9dbe5
Yong Chee <yong.chee@sap.com>
2018-01-02 15:53:56 -0500

    Component Routing - Understanding routerLink

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
+      <a routerLink="/">
+        Home
+      </a>
+      <a routerLink="/oops">
+        404
+      </a>
       <router-outlet></router-outlet>
     </div>
   `
--- app/not-found.component.ts
+++ app/not-found.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
   selector: "not-found",
   template: `
     <div>
-      Not found
+      Not found, <a routerLink="/">go home</a>?
     </div>
   `
 })
