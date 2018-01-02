ac3ca8f7b1ad82195b059120b0c4eaadbe006ea4
Yong Chee <yong.chee@sap.com>
2018-01-02 18:01:10 -0500

    Component Routing - Applying redirects

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,7 +11,7 @@ import { NotFoundComponent } from "./not-found.component";
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: '', redirectTo: 'passengers', pathMatch: 'full'},
   { path: '**', component: NotFoundComponent}
 ];
 
