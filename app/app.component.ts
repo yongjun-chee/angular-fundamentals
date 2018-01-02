36ad706dd426e7ddacd965143dd5b9b15bd98222
Yong Chee <yong.chee@sap.com>
2018-01-02 16:17:11 -0500

    Component Routing - Dynamic navigation with ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Nav {
+  link: string,
+  name: string,
+  exact: boolean
+}
+
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
@@ -7,19 +13,28 @@ import { Component } from '@angular/core';
     <div class="app">
       <nav class="nav">
         <a 
-          routerLink="/"
+          *ngFor="let item of nav"
+          [routerLink]="item.link"
           routerLinkActive="active"
-          [routerLinkActiveOptions]="{ exact: true }">
-          Home
-        </a>
-        <a
-          routerLink="/oops"
-          routerLinkActive="active">
-          404
+          [routerLinkActiveOptions]="{ exact: item.exact }">
+          {{ item.name }}
         </a>
       </nav>
       <router-outlet></router-outlet>
     </div>
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  nav: Nav[] = [
+    {
+      link: "/",
+      name: "Home",
+      exact: true
+    },
+    {
+      link: "/oops",
+      name: "404",
+      exact: false
+    }
+  ];
+}
