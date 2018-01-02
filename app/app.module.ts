40d6c50780c9f5ad92d50dc17ae6a05a26163559
Yong Chee <yong.chee@sap.com>
2018-01-02 17:57:38 -0500

    Component Routing - Hash location strategy

--- app/app.module.ts
+++ app/app.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     CommonModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true }),
     PassengerDashboardModule
   ],
   bootstrap: [
